a7cc0500a4666f876345fbd708a8a2b223d4ba70
unknown <rony120114@gmail.com>
2023-09-23 16:18:40 +0600

    polishing

--- src/Pages/signup/index.js
+++ src/Pages/signup/index.js
@@ -47,7 +47,7 @@ const Signup = () => {
 
     try {
       await dispatch(signup(user));
-        // router.push('/login');
+        router.push('/login');
     } catch (error) {
         console.error('Error during signup:', error);
     }
